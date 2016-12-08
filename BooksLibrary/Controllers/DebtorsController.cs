71f202ddb6e13f59b060bac962643ed53571f7ce
Xolm <Xolm@DESKTOP-S8I5MFE>
2016-12-08 20:23:15 +0300

    some changes

---
+++ BooksLibrary/Controllers/DebtorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksLibrary.Controllers
+{
+    public class DebtorsController : Controller
+    {
+        // GET: Debtors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
